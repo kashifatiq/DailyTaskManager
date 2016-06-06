faed9e78f28192880ad9dab2e046ee12c6a40cde
kashif <kashifatiq@users.noreply.github.com>
2016-06-06 12:15:02 +0500

    Update Program.cs

--- DailyTaskManager/DailyTaskManager/Program.cs
+++ DailyTaskManager/DailyTaskManager/Program.cs
@@ -14,6 +14,7 @@ namespace DailyTaskManager
         [STAThread]
         static void Main()
         {
+            //
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmTasksList());
