88ad335c4dbd1617baa0788c9e5d68616d54fc19
kashif <kashif_atiq@hotmail.com>
2016-06-12 22:16:36 +0500

    3

--- DailyTaskManager/DailyTaskManager/Program.cs
+++ DailyTaskManager/DailyTaskManager/Program.cs
@@ -14,7 +14,7 @@ namespace DailyTaskManager
         [STAThread]
         static void Main()
         {
-            // test 2
+            // test 3
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmTasksList());
